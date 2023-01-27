8a83857529bbc6f14eaf7b06066db5120cca5815
Matthew Sharpe <matthew.sharpe@enable.com>
2023-01-27 22:30:04 +0000

    wip

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,5 +1,12 @@
-import { LinterResult } from 'stylelint';
+import { LinterResult, LintResult } from 'stylelint';
 import formatter from '../src/index';
+
+const emptyResult: LintResult = {
+    deprecations: [],
+    invalidOptionWarnings: [],
+    parseErrors: [],
+    warnings: []
+};
 
 const linterResult: LinterResult = {
     cwd: '',
@@ -11,8 +18,21 @@ const linterResult: LinterResult = {
 };
 
 describe('Formatter', () => {
-    it('should return a string', () => {
+    it('should return empty string for no results', () => {
         const result = formatter([], linterResult);
         expect(typeof result).toBe('string');
+        expect(result).toBe('');
+    });
+
+    it('should return a string', () => {
+        const result = formatter([emptyResult], linterResult);
+        expect(typeof result).toBe('string');
+    });
+
+    // Should probably be per warning..!
+    it('should return a string with a line per result', () => {
+        const result = formatter([emptyResult, emptyResult, emptyResult], linterResult);
+        const lineCount = result.split(/\r\n|\r|\n/).length;
+        expect(lineCount).toBe(3);
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Formatter } from 'stylelint';
 
 const formatter: Formatter = (results, returnValue): string => {
+    if (Array.isArray(results) && results.length === 0) {
+        return '';
+    }
+
     return "a string of formatted results";
 };
 
