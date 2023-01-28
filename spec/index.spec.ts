33f3eb53309ebf113b6ccbcf0d43391fa8386059
Matt Sharpe <mds@live.co.uk>
2023-01-28 16:08:15 +0000

    wip

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -8,7 +8,14 @@ const emptyResult: LintResult = { deprecations: [], invalidOptionWarnings: [], p
 
 const singleWarningResult: LintResult = {
     deprecations: [], invalidOptionWarnings: [], parseErrors: [], warnings: [
-        { line: 1, column: 1, rule: 'rule', severity: 'error', text: 'lorem ipsum' }
+        { line: 1, column: 1, rule: 'rule', severity: 'error', text: 'lorem' }
+    ]
+};
+
+const twoWarningResult: LintResult = {
+    deprecations: [], invalidOptionWarnings: [], parseErrors: [], warnings: [
+        { line: 1, column: 1, rule: 'rule', severity: 'error', text: 'lorem' },
+        { line: 2, column: 1, rule: 'rule', severity: 'error', text: 'ipsum' }
     ]
 };
 
@@ -45,5 +52,17 @@ describe('Formatter', () => {
         const result = formatter([singleWarningResult, singleWarningResult], linterResult);
         const lineCount = result.split(EOL).length;
         expect(lineCount).toBe(2);
+    });
+
+    it('should return two lines for one result, two warnings', () => {
+        const result = formatter([twoWarningResult], linterResult);
+        const lineCount = result.split(EOL).length;
+        expect(lineCount).toBe(2);
+    });
+
+    it('should return three lines for two result, three warnings', () => {
+        const result = formatter([twoWarningResult, singleWarningResult], linterResult);
+        const lineCount = result.split(EOL).length;
+        expect(lineCount).toBe(3);
     });
 });
