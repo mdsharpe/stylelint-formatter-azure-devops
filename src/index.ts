b2ce0400a0dff4194d6da8ab2209a77b15ebba60
Matt Sharpe <mds@live.co.uk>
2023-01-28 20:16:38 +0000

    wip

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import { type Formatter } from 'stylelint';
 import formatMessage from './formatMessage';
 
 const formatter: Formatter = (results, _): string => {
-    if (!Array.isArray(results)) {
+    if (!Array.isArray(results) || results.length === 0) {
         return '';
     }
 
-    results = results.filter(result => Array.isArray(result.warnings) && result.warnings.length > 0);
+    results = results.filter(o => Array.isArray(o.warnings) && o.warnings.length > 0);
 
     if (results.length === 0) {
         return '';
@@ -20,7 +20,7 @@ const formatter: Formatter = (results, _): string => {
             warning
         })));
 
-    const formattedResults = warnings.map(warning => formatMessage(warning.warning, warning.source));
+    const formattedResults = warnings.map(o => formatMessage(o.warning, o.source));
 
     return formattedResults.join(EOL);
 };
